bd2cdbfeff6c2c2ec2443b3b92eabe2d641c2205
DerMann1 <86832352+DerMann1@users.noreply.github.com>
2021-08-15 14:50:50 +1000

    Update messages.js
    Adding the actual code

--- messages.js
+++ messages.js
@@ -1,1 +1,40 @@
-changes
+let johannesburg = 'Johannesburg';
+let harare = 'Harare';
+let nairobi = 'Nairobi';
+let lagos = 'Lagos';
+let accra = 'Accra'
+let lusaka = 'Lusaka'
+let kinshasa = 'kinshasa'
+
+function generator() {
+    let number = Math.floor(Math.random() * 8)
+    switch (number) {
+        case 1:
+            console.log(johannesburg);
+            break;
+
+        case 2:
+            console.log(harare);
+            break;
+
+        case 3:
+            console.log(nairobi)
+            break;
+
+        case 4:
+            console.log(lagos)
+            break;
+
+        case 5:
+            console.log(accra)
+            break;
+
+        case 6:
+            console.log(lusaka);
+            break;
+
+        case 7:
+            console.log(kinshasa)
+            break;
+    }
+}
